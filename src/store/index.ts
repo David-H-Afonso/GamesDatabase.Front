2f1176c70b34c9c1b112e9e7ff63bf527c2d82b1
David Hormiga Afonso <davidhormigafonso@gmail.com>
2025-09-18 18:08:37 +0200

    feat: update store configuration to include new reducers for games, game status, game platform, game play options, game played status, and theme

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { persistStore, persistReducer } from 'redux-persist'
 import { combineReducers } from '@reduxjs/toolkit'
 import storage from 'redux-persist/lib/storage'
-import { exampleFeatureReducer } from './features/exampleFeature'
+import gamesReducer from './features/games/gamesSlice'
+import gameStatusReducer from './features/gameStatus/gameStatusSlice'
+import gamePlatformReducer from './features/gamePlatform/gamePlatformSlice'
+import gamePlayWithReducer from './features/gamePlayWith/gamePlayWithSlice'
+import gamePlayedStatusReducer from './features/gamePlayedStatus/gamePlayedStatusSlice'
+import themeReducer from './features/theme/themeSlice'
 
 /**
  * CENTRALIZED PERSISTENCE CONFIGURATION
@@ -15,12 +20,17 @@ import { exampleFeatureReducer } from './features/exampleFeature'
 const persistConfig = {
 	key: 'root',
 	storage,
-	whitelist: ['exampleFeature'], // Add reducers here to persist
+	whitelist: ['games', 'gameStatus', 'gamePlatform', 'gamePlayWith', 'gamePlayedStatus', 'theme'], // Add reducers here to persist
 }
 
 // Combine reducers - Add your reducers here
 const rootReducer = combineReducers({
-	exampleFeature: exampleFeatureReducer,
+	games: gamesReducer,
+	gameStatus: gameStatusReducer,
+	gamePlatform: gamePlatformReducer,
+	gamePlayWith: gamePlayWithReducer,
+	gamePlayedStatus: gamePlayedStatusReducer,
+	theme: themeReducer,
 })
 
 // Create persisted reducer - Single point of persistence configuration
